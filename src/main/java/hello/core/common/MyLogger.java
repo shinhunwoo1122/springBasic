2ec46fa3762e7d2247acb9dcd8736242c94a321d
shinhunwoo <shinhunwoo1122@gmail.com>
2023-05-07 22:22:43 +0900

    - git 확인

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Component
 @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
-    //git 소스 테스트
+    //git 소스 테스트1
     private String uuid;
     private String requestURL;
 
